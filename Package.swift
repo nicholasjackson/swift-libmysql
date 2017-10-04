e9cf8966c6782a42835333293486e7563a7106bf
Nic Jackson <jackson.nic@gmail.com>
2017-10-04 16:20:21 +0100

    Added package hints

--- Package.swift
+++ Package.swift
@@ -1,5 +1,12 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "swift-libmysql"
+    name: "swift-libmysql",
+
+    pkgConfig: "mysqlclient",
+    providers: [
+        .brew(["mysql"]),
+        .apt(["libmysqlclient-dev"])
+    ]
 )
